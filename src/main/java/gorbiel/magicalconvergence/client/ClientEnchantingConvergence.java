082be4bf5b2cf8661c8340abae394cc1830c35a7
Gabriel Kania <gracz1212yt@gmail.com>
2026-04-01 21:40:16 +0200

    port client package

---
+++ src/main/java/gorbiel/magicalconvergence/client/ClientEnchantingConvergence.java
@@ -0,0 +1,29 @@
+package gorbiel.magicalconvergence.client;
+
+import gorbiel.magicalconvergence.client.gui.screen.ScreenEnchantingConvergence;
+import gorbiel.magicalconvergence.init.EnchantingConvergenceContainers;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.MenuScreens;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+public class ClientEnchantingConvergence {
+
+	public static void setupClient(FMLClientSetupEvent event) {
+		event.enqueueWork(() ->
+				MenuScreens.register(
+						EnchantingConvergenceContainers.ENCHANTING_TABLE.get(),
+						ScreenEnchantingConvergence::new
+				)
+		);
+	}
+
+	@SuppressWarnings("resource")
+	@OnlyIn(Dist.CLIENT)
+	public static Player getPlayer() {
+		return Minecraft.getInstance().player;
+	}
+
+}
